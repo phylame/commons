0c6e2ad4b3835672bef9c3da461cc93442d99ba9
wp <phylame@163.com>
2018-06-21 20:52:46 +0800

    StringUtils: add trim & remove functions

--- src/main/java/pw/phylame/commons/text/StringUtils.java
+++ src/main/java/pw/phylame/commons/text/StringUtils.java
@@ -5,6 +5,8 @@ import lombok.NonNull;
 import java.util.Iterator;
 
 /**
+ * Utilities for string.
+ *
  * @author wp <phylame@163.com>
  * @date 2018/06/08
  */
@@ -15,6 +17,35 @@ public final class StringUtils {
 
     public static boolean isNotEmpty(CharSequence cs) {
         return cs != null && cs.length() != 0;
+    }
+
+    public static String trim(String str) {
+        return str != null ? str.trim() : null;
+    }
+
+    public static String trimToNull(String str) {
+        str = trim(str);
+        return isNotEmpty(str) ? str : null;
+    }
+
+    public static String removeStart(String str, String prefix) {
+        if (isEmpty(str) || isEmpty(prefix)) {
+            return str;
+        } else if (str.startsWith(prefix)) {
+            return str.substring(prefix.length());
+        } else {
+            return str;
+        }
+    }
+
+    public static String removeEnd(String str, String suffix) {
+        if (isEmpty(str) || isEmpty(suffix)) {
+            return str;
+        } else if (str.endsWith(suffix)) {
+            return str.substring(0, str.length() - suffix.length());
+        } else {
+            return str;
+        }
     }
 
     public static <E> String join(@NonNull Iterable<E> items, CharSequence separator) {
