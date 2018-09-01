dc34f365f5aa08a194af4a84c563810813d40a22
wp <phylame@163.com>
2018-09-01 15:16:28 +0800

    new utils for string

--- src/main/java/pw/phylame/commons/text/StringUtils.java
+++ src/main/java/pw/phylame/commons/text/StringUtils.java
@@ -16,6 +16,8 @@ import java.util.function.Supplier;
  * @date 2018/06/08
  */
 public final class StringUtils {
+    public static final String EMPTY = "";
+
     public static boolean isEmpty(CharSequence text) {
         return text == null || text.length() == 0;
     }
@@ -29,7 +31,7 @@ public final class StringUtils {
         if (text == null || (length = text.length()) == 0) {
             return true;
         }
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i != length; ++i) {
             if (!Character.isWhitespace(text.charAt(i))) {
                 return false;
             }
@@ -42,10 +44,11 @@ public final class StringUtils {
     }
 
     public static boolean isLowerCase(CharSequence text) {
-        if (isEmpty(text)) {
+        int length;
+        if (text == null || (length = text.length()) == 0) {
             return false;
         }
-        for (int i = 0, end = text.length(); i != end; ++i) {
+        for (int i = 0; i != length; ++i) {
             if (!Character.isLowerCase(text.charAt(i))) {
                 return false;
             }
@@ -54,10 +57,11 @@ public final class StringUtils {
     }
 
     public static boolean isUpperCase(CharSequence text) {
-        if (isEmpty(text)) {
+        int length;
+        if (text == null || (length = text.length()) == 0) {
             return false;
         }
-        for (int i = 0, end = text.length(); i != end; ++i) {
+        for (int i = 0; i != length; ++i) {
             if (!Character.isUpperCase(text.charAt(i))) {
                 return false;
             }
@@ -65,31 +69,106 @@ public final class StringUtils {
         return true;
     }
 
-    public static int count(String text, String target) {
-        if (isEmpty(text) || isEmpty(target)) {
-            return 0;
+    public static boolean isAlpha(CharSequence text) {
+        if (isEmpty(text)) {
+            return false;
         }
-
-        int count = 0;
-        int index = 0;
-        val length = target.length();
-        while ((index = text.indexOf(target, index)) != -1) {
-            index += length;
-            count++;
+        val length = text.length();
+        for (int i = 0; i != length; ++i) {
+            if (!Character.isLetter(text.charAt(i))) {
+                return false;
+            }
         }
-
-        return count;
+        return true;
     }
 
-    public static int size(int x) {
+    public static boolean isAlphaSpace(CharSequence text) {
+        if (text == null) {
+            return false;
+        }
+        val length = text.length();
+        for (int i = 0; i != length; ++i) {
+            if (!Character.isLetter(text.charAt(i)) && text.charAt(i) != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isAlphanumeric(CharSequence text) {
+        if (isEmpty(text)) {
+            return false;
+        }
+        val length = text.length();
+        for (int i = 0; i != length; ++i) {
+            if (!Character.isLetterOrDigit(text.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isAlphanumericSpace(CharSequence text) {
+        if (text == null) {
+            return false;
+        }
+        val length = text.length();
+        for (int i = 0; i != length; ++i) {
+            if (!Character.isLetterOrDigit(text.charAt(i)) && text.charAt(i) != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isNumeric(CharSequence text) {
+        if (isEmpty(text)) {
+            return false;
+        }
+        val length = text.length();
+        for (int i = 0; i != length; ++i) {
+            if (!Character.isDigit(text.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isNumericSpace(CharSequence text) {
+        if (text == null) {
+            return false;
+        }
+        val length = text.length();
+        for (int i = 0; i != length; ++i) {
+            if (!Character.isDigit(text.charAt(i)) && text.charAt(i) != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isWhitespace(CharSequence text) {
+        if (text == null) {
+            return false;
+        }
+        val length = text.length();
+        for (int i = 0; i != length; ++i) {
+            if (!Character.isWhitespace(text.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int size(int n) {
         int d = 1;
-        if (x >= 0) {
+        if (n >= 0) {
             d = 0;
-            x = -x;
+            n = -n;
         }
         int p = -10;
-        for (int i = 1; i < 10; i++) {
-            if (x > p) {
+        for (int i = 1; i != 10; ++i) {
+            if (n > p) {
                 return i + d;
             }
             p = 10 * p;
@@ -97,12 +176,26 @@ public final class StringUtils {
         return 10 + d;
     }
 
-    public static String repeat(CharSequence text, int count) {
-        val b = new StringBuilder();
-        for (int i = 0; i < count; i++) {
-            b.append(text);
+    public static int count(String text, String target) {
+        if (isEmpty(text) || isEmpty(target)) {
+            return 0;
         }
-        return b.toString();
+        int count = 0;
+        int index = 0;
+        val length = target.length();
+        while ((index = text.indexOf(target, index)) != -1) {
+            index += length;
+            ++count;
+        }
+        return count;
+    }
+
+    public static String repeat(CharSequence text, int count) {
+        val sb = new StringBuilder();
+        for (int i = 0; i != count; ++i) {
+            sb.append(text);
+        }
+        return sb.toString();
     }
 
     public static String coalesce(String text, String emptyDefault) {
@@ -118,15 +211,13 @@ public final class StringUtils {
         if (text == null || (end = text.length()) == 0) {
             return text;
         }
-
         int begin = 0;
-        while (begin < end && Character.isWhitespace(text.charAt(begin))) {
-            begin++;
+        while (begin != end && Character.isWhitespace(text.charAt(begin))) {
+            ++begin;
         }
-        while (begin < end && Character.isWhitespace(text.charAt(end - 1))) {
-            end--;
+        while (begin != end && Character.isWhitespace(text.charAt(end - 1))) {
+            --end;
         }
-
         return text.substring(begin, end);
     }
 
@@ -136,35 +227,77 @@ public final class StringUtils {
     }
 
     public static String trimToEpmty(String text) {
-        return text == null ? "" : trim(text);
+        return text == null ? EMPTY : trim(text);
     }
 
     public static String trimStart(String text) {
-        int end;
-        if (text == null || (end = text.length()) == 0) {
-            return text;
-        }
-
-        int begin = 0;
-        while (begin < end && Character.isWhitespace(text.charAt(begin))) {
-            ++begin;
-        }
-
-        return text.substring(begin);
+        return stripStart(text, null);
     }
 
     public static String trimEnd(String text) {
+        return stripEnd(text, null);
+    }
+
+    public static String strip(String str) {
+        return strip(str, null);
+    }
+
+    public static String stripToNull(String str) {
+        if (str == null) {
+            return null;
+        }
+        str = strip(str, null);
+        return str.isEmpty() ? null : str;
+    }
+
+    public static String stripToEmpty(String str) {
+        return str == null ? EMPTY : strip(str, null);
+    }
+
+    public static String strip(String str, String stripChars) {
+        if (isEmpty(str)) {
+            return str;
+        }
+        return stripEnd(stripStart(str, stripChars), stripChars);
+    }
+
+    public static String stripStart(String str, String stripChars) {
         int length;
-        if (text == null || (length = text.length()) == 0) {
-            return text;
+        if (str == null || (length = str.length()) == 0) {
+            return str;
         }
-
-        int end = length - 1;
-        while (end >= 0 && Character.isWhitespace(text.charAt(end))) {
-            --end;
+        int begin = 0;
+        if (stripChars == null) {
+            while (begin != length && Character.isWhitespace(str.charAt(begin))) {
+                ++begin;
+            }
+        } else if (stripChars.isEmpty()) {
+            return str;
+        } else {
+            while (begin != length && stripChars.indexOf(str.charAt(begin)) != -1) {
+                ++begin;
+            }
         }
+        return str.substring(begin);
+    }
 
-        return text.substring(0, end);
+    public static String stripEnd(String str, String stripChars) {
+        int end;
+        if (str == null || (end = str.length()) == 0) {
+            return str;
+        }
+        if (stripChars == null) {
+            while (end != 0 && Character.isWhitespace(str.charAt(end - 1))) {
+                --end;
+            }
+        } else if (stripChars.isEmpty()) {
+            return str;
+        } else {
+            while (end != 0 && stripChars.indexOf(str.charAt(end - 1)) != -1) {
+                --end;
+            }
+        }
+        return str.substring(0, end);
     }
 
     public static String removeStart(String text, String prefix) {
@@ -187,23 +320,23 @@ public final class StringUtils {
         }
     }
 
-    public static String replace(String text, String target, @NonNull String replacement, int limit) {
+    public static String replace(String text, String target, String replacement, int limit) {
         if (isEmpty(text) || isEmpty(target) || limit <= 0) {
             return text;
         }
         var begin = 0;
         val length = target.length();
-        val b = new StringBuilder();
-        for (int i = 0; i < limit; i++) {
+        val sb = new StringBuilder();
+        for (int i = 0; i != limit; ++i) {
             val index = text.indexOf(target, begin);
             if (index < 0) {
                 break;
             }
-            b.append(text, begin, index).append(replacement);
+            sb.append(text, begin, index).append(replacement);
             begin = index + length;
         }
-        b.append(text.substring(begin));
-        return b.toString();
+        sb.append(text.substring(begin));
+        return sb.toString();
     }
 
     public static String replaceFirst(String text, String target, @NonNull String replacement) {
@@ -226,14 +359,97 @@ public final class StringUtils {
                 : text;
     }
 
-    public static String[] split(@NonNull String text, @NonNull String separator, int limit) {
+    public static String substring(String str, int begin) {
+        if (str == null) {
+            return null;
+        }
+        if (begin < 0) {
+            begin = str.length() + begin;
+        }
+        if (begin < 0) {
+            begin = 0;
+        }
+        if (begin > str.length()) {
+            return EMPTY;
+        }
+        return str.substring(begin);
+    }
+
+    public static String substring(String str, int begin, int end) {
+        if (str == null) {
+            return null;
+        }
+        if (end < 0) {
+            end = str.length() + end;
+        }
+        if (begin < 0) {
+            begin = str.length() + begin;
+        }
+        if (end > str.length()) {
+            end = str.length();
+        }
+        if (begin > end) {
+            return EMPTY;
+        }
+        if (begin < 0) {
+            begin = 0;
+        }
+        if (end < 0) {
+            end = 0;
+        }
+        return str.substring(begin, end);
+    }
+
+    public static String left(String str, int len) {
+        if (str == null) {
+            return null;
+        }
+        if (len < 0) {
+            return EMPTY;
+        }
+        if (str.length() <= len) {
+            return str;
+        }
+        return str.substring(0, len);
+    }
+
+    public static String right(String str, int len) {
+        if (str == null) {
+            return null;
+        }
+        if (len < 0) {
+            return EMPTY;
+        }
+        if (str.length() <= len) {
+            return str;
+        }
+        return str.substring(str.length() - len);
+    }
+
+    public static String mid(String str, int off, int len) {
+        if (str == null) {
+            return null;
+        }
+        if (len < 0 || off > str.length()) {
+            return EMPTY;
+        }
+        if (off < 0) {
+            off = 0;
+        }
+        if (str.length() <= off + len) {
+            return str.substring(off);
+        }
+        return str.substring(off, off + len);
+    }
+
+    public static String[] split(String text, String separator, int limit) {
         if (limit <= 0) {
             return text.split(separator);
         }
         int i, pos = 0;
         val width = separator.length();
         val results = new ArrayList<String>(limit);
-        for (i = 1; i < limit; ++i) {
+        for (i = 1; i != limit; ++i) {
             val begin = text.indexOf(separator, pos);
             if (begin == -1) {
                 results.add(text.substring(pos));
@@ -250,14 +466,14 @@ public final class StringUtils {
 
     public static Pair<String, String> partition(String text, String separator) {
         if (isEmpty(text)) {
-            return Pair.of("", "");
+            return Pair.of(EMPTY, EMPTY);
         } else if (isEmpty(separator)) {
-            return Pair.of(text, "");
+            return Pair.of(text, EMPTY);
         }
         val index = text.indexOf(separator);
         return index != -1
                 ? Pair.of(text.substring(0, index), text.substring(index + separator.length()))
-                : Pair.of(text, "");
+                : Pair.of(text, EMPTY);
     }
 
     public static String getFirst(String text, String separator) {
@@ -267,7 +483,7 @@ public final class StringUtils {
             return text;
         }
         val index = text.indexOf(separator);
-        return index != -1 ? text.substring(0, index) : "";
+        return index != -1 ? text.substring(0, index) : EMPTY;
     }
 
     public static String getSecond(String text, String separator) {
@@ -277,10 +493,10 @@ public final class StringUtils {
             return text;
         }
         val index = text.indexOf(separator);
-        return index != -1 ? text.substring(index + separator.length()) : "";
+        return index != -1 ? text.substring(index + separator.length()) : EMPTY;
     }
 
-    public static <E> String join(@NonNull Iterable<E> items, @NonNull CharSequence separator) {
+    public static <E> String join(Iterable<E> items, CharSequence separator) {
         return StringJoiner.<E>builder()
                 .iterator(items.iterator())
                 .separator(separator)
@@ -288,12 +504,35 @@ public final class StringUtils {
                 .join();
     }
 
-    public static <E> String join(@NonNull Iterator<E> iterator, @NonNull CharSequence separator) {
+    public static <E> String join(Iterator<E> iterator, CharSequence separator) {
         return StringJoiner.<E>builder()
                 .iterator(iterator)
                 .separator(separator)
                 .build()
                 .join();
+    }
+
+    public static String capitalize(String str) {
+        int length;
+        if (str == null || (length = str.length()) == 0) {
+            return str;
+        }
+
+        val firstCodePoint = str.codePointAt(0);
+        val newCodePoint = Character.toTitleCase(firstCodePoint);
+        if (firstCodePoint == newCodePoint) {
+            return str;
+        }
+
+        int outOffset = 0;
+        val codePoints = new int[length];
+        codePoints[outOffset++] = newCodePoint;
+        for (int inOffset = Character.charCount(firstCodePoint); inOffset < length; ) {
+            val codePoint = str.codePointAt(inOffset);
+            codePoints[outOffset++] = codePoint;
+            inOffset += Character.charCount(codePoint);
+        }
+        return new String(codePoints, 0, outOffset);
     }
 
     public static String getValue(String text, String name, String partSeparator) {
@@ -304,7 +543,7 @@ public final class StringUtils {
         return getValue(text, name, "=", partSeparator, caseSensitive);
     }
 
-    public static String getValue(String text, @NonNull String name, @NonNull String valueSeparator, @NonNull String partSeparator, boolean caseSensitive) {
+    public static String getValue(String text, String name, String valueSeparator, String partSeparator, boolean caseSensitive) {
         if (isEmpty(text)) {
             return null;
         }
