b982f9e6baeaf11cabba62ef5ef6e32a34df7bf7
wp <phylame@163.com>
2018-09-10 22:36:37 +0800

    add supplier for worker value

--- src/main/java/pw/phylame/commons/value/Worker.java
+++ src/main/java/pw/phylame/commons/value/Worker.java
@@ -1,14 +1,20 @@
 package pw.phylame.commons.value;
 
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import pw.phylame.commons.NestedException;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.function.Supplier;
 
-public abstract class Worker<T> implements Value<T> {
+@RequiredArgsConstructor
+public final class Worker<T> implements Value<T> {
+    @NonNull
+    private final Supplier<T> supplier;
+
     private T value;
 
     private volatile boolean done = false;
@@ -17,23 +23,35 @@ public abstract class Worker<T> implements Value<T> {
 
     private final Object lock = new Object();
 
-    private final Callable<T> action = () -> {
+    private final Callable<T> action = this::handleGet;
+
+    private T handleGet() {
         if (!done) {
             synchronized (lock) {
                 if (!done) {
-                    value = handleGet();
+                    value = supplier.get();
                     future = null;
                     done = true;
                 }
             }
         }
         return value;
-    };
+    }
 
-    protected abstract T handleGet();
+    public void schedule(@NonNull ExecutorService service) {
+        if (future != null) {
+            throw new IllegalStateException("Already submitted");
+        }
+        future = service.submit(action);
+    }
+
+    public void reset() {
+        done = false;
+        future = null;
+    }
 
     @Override
-    public final T get() {
+    public T get() {
         if (done) {
             return value;
         } else if (future != null) {
@@ -49,17 +67,5 @@ public abstract class Worker<T> implements Value<T> {
                 throw new NestedException(e);
             }
         }
-    }
-
-    public final void schedule(@NonNull ExecutorService service) {
-        if (future != null) {
-            throw new IllegalStateException("Already submitted");
-        }
-        future = service.submit(action);
-    }
-
-    public final void reset() {
-        done = false;
-        future = null;
     }
 }
